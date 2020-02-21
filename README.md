7bac5010c89986823b3c12641b54f3818914b6c6
Adrian Sutherland <adrian@sutherlandonline.org>
2020-02-21 13:04:36 +0000

    v1.4.1

--- README.md
+++ README.md
@@ -2,19 +2,17 @@
 Docker container providing an emulated S/370 mainframe (Hercules) running VM/370 based on the six-pack distribution.
 
 # Releases
-
 The Docker images are here
     https://hub.docker.com/repository/docker/adriansutherland/vm370
 
 See the release tab for Dockerfile build source and the VM/370 DASD files.
 
-
-latest -> 1.3.7
-
-Version 1.3.4 corresponds to Six Pack 1.3 Beta 3.
+# History
+- latest -> 1.4.1
+- Version 1.4.x has latest GCCLIB builds
+- Version 1.3.4 corresponds to Six Pack 1.3 Beta 3.
 
 See changelog.txt for full version history
-
 
 # Links
 - Six pack distribution - http://www.smrcc.org.uk/members/g4ugm/VM370.htm
--- build.sh
+++ build.sh
@@ -28,15 +28,15 @@ herccontrol "/logoff" -w "^VM/370 Online"
 herccontrol "/logon operator operator" -w "RECONNECTED AT"
 herccontrol "/shutdown" -w "^HHCCP011I"
 
-# YATA - Ubuntu
-wget -nv https://github.com/adesutherland/yata/releases/download/v1.1.3/YATA-Ubuntu.zip
+# YATA (1.2.0) - Ubuntu
+wget -nv https://github.com/adesutherland/yata/releases/download/v1.2.0/YATA-Ubuntu.zip
 unzip YATA-Ubuntu.zip
 chmod +x YATA-Ubuntu/yata
 cp YATA-Ubuntu/yata /usr/local/bin
 rm -r YATA-Ubuntu
 rm YATA-Ubuntu.zip
 
-# YATA CMS v 1.3.1 (Last version that works on GCCLIB pre-0.7.x)
+# YATA CMS v 1.1.3 (Last version that works on GCCLIB pre-0.7.x)
 wget -nv https://github.com/adesutherland/yata/releases/download/v1.1.3/YATA-CMS.zip
 unzip YATA-CMS.zip
 chmod +x YATA-CMS/cmsinstall.sh
@@ -50,7 +50,7 @@ rm -r io
 rm -r YATA-CMS
 rm YATA-CMS.zip
 
-# GCCLIB (0.7.1)
+# GCCLIB (0.7.1) - needs a version of YATA to download
 wget -nv https://github.com/adesutherland/CMS-370-GCCLIB/releases/download/v0.7.1/GCCLIB.zip
 unzip GCCLIB.zip
 chmod +x GCCLIB/cmsinstall.sh
@@ -63,6 +63,20 @@ cd ..
 rm -r io
 rm -r GCCLIB
 rm GCCLIB.zip
+
+# YATA CMS v 1.2.0 (Uptodate version works with latest GCCLIB)
+wget -nv https://github.com/adesutherland/yata/releases/download/v1.2.0/YATA-CMS.zip
+unzip YATA-CMS.zip
+chmod +x YATA-CMS/cmsinstall.sh
+mkdir io
+cp YATA-CMS/cmsinstall.sh io
+cp YATA-CMS/yatabin.aws io
+cd io
+./cmsinstall.sh
+cd ..
+rm -r io
+rm -r YATA-CMS
+rm YATA-CMS.zip
 
 # Compress disks
 # hercules -f hercules.conf -d >/dev/null 2>/dev/null &

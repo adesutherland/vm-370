a90a680607ed10204c8644d282f797e4396cf6c7
Adrian Sutherland <adrian@sutherlandonline.org>
2020-09-19 18:24:41 +0100

    execio, hrc406ds, hrc407ds, hrc700dk

---
+++ mods/execio/install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# EXECIO Install
+# Predicate - logged on as MAINT
+#           - Needs regen.sh run afterwards
+
+# Exit if there is an error
+set -e
+
+# Send YATA TXT
+herccontrol "/purge rdr" -w "^Ready;"
+yata -c
+herccontrol -m >tmp; read mark <tmp; rm tmp
+echo "USERID  MAINT\n:READ  YATA     TXT     " > tmp
+
+cat yata.txt >> tmp
+netcat -q 0 localhost 3505 < tmp
+rm tmp
+herccontrol -w "HHCRD012I" -f $mark
+herccontrol "/" -w "RDR FILE"
+herccontrol "/read *" -w "^Ready;"
+herccontrol "/yata -x" -w "^Ready;"
+herccontrol "/erase yata txt a" -w "^Ready;"
+
+# Install Application
+herccontrol "/install" -w "^Ready;"
+herccontrol "/erase install exec a" -w "^Ready;"
